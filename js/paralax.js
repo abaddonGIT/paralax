b338b0fa33cff4027f12d81f1378d994a36ed9c0
abaddonGIT <abaddongit@gmail.com>
2014-01-13 17:38:29 +0400

    То что было переписано на стратегии для дальнейшего расширения

--- js/paralax.js
+++ js/paralax.js
@@ -8,73 +8,114 @@
     "use strict";
     var w = window, d = document, $W = $(w), $D = $(d), $el = null, that = null,
         support,
+        queryApi,
+        config = {},
         blocks = null,
         ln = null,
-    //размеры окна
+        //размеры окна
+        innerWidth = null,
+        innerHeight = null;
+
+    var Paralax = function (strategy) {
+        this.strategy = strategy;
+    };
+
+    Paralax.prototype.start = function (el, defOption) {
+        return this.strategy.init(el, defOption);
+    };
+
+    //Что-то типа абстрактного класса
+    var GetParalax = function () {};
+
+    //Описываем необходимые методы
+    GetParalax.prototype.init = function (el, options) {
+        that = el;
+        var thisStyle = that.style;
+        //Проверка на поддержку css-transform
+        support = thisStyle.transition !== undefined || thisStyle.WebkitTransition !== undefined || thisStyle.MozTransition !== undefined || thisStyle.MsTransition !== undefined || thisStyle.OTransition !== undefined;
+        queryApi = d.querySelector || false;
+        config = options;
         innerWidth = w.innerWidth || d.documentElement.clientWidth,
         innerHeight = w.innerHeight || d.documentElement.clientHeight;
+        
+        if (!blocks) {
+            blocks = d.querySelectorAll(config.layerClass);
+            ln = blocks.length;
+        }
+        this.go();
+    };
 
-    var Paralax = function (el, options) {
-        $el = $(el);
-        that = el;
+    GetParalax.prototype.go = function () {};
+    /*
+    * Получение координатов мыши
+    */
+    GetParalax.prototype.getXY = function (e) {
+        //если IE 
+        if (!e.pageX) {
+            var html = d.documentElement;
+            var body = d.body;
 
-        this.init = function () {
-            this.declareVars();
-            this.bindEvent();
+            e.pageX = e.clientX + (html && html.scrollLeft || body && body.scrollLeft || 0) - (html.clientLeft || 0);
+            e.pageY = e.clientY + (html && html.scrollTop || body && body.scrollTop || 0) - (html.clientTop || 0);
+        }
+        return { 'mouseX': e.pageX, 'mouseY': e.pageY };
+    };
+    
+    /*
+    * Регистрирует событие
+    */
+    GetParalax.prototype.addEvent = function (type, el, func) {
+        el.addEventListener(type, function (event) {
+            var e = event || w.event;
+            func.call(this, e);
+        }, false);
+    };
+
+    /*
+    * Определяет размеры блока по контенту
+    */
+    GetParalax.prototype.sizes = function (el) {
+        return sizes = {
+            height: el.outerHeight(),
+            width: el.outerWidth()
         };
+    };
 
+    //Базовая стратегия
+    var BaseParalaxStr = function () {
         /*
-        * Объевление переменных
+        * Запускаем паралакс
         */
-        this.declareVars = function () {
-            //Определяем поддержывает ли браузер свойство transition
-            var thisStyle = that.style;
-            support = thisStyle.transition !== undefined || thisStyle.WebkitTransition !== undefined || thisStyle.MozTransition !== undefined || thisStyle.MsTransition !== undefined || thisStyle.OTransition !== undefined;
-
-            //Находим все внутренние блоки
-            if (!blocks) {
-                blocks = d.querySelectorAll(options.layerClass);
-                ln = blocks.length;
-            }
-        };
-        /*
-        * Вешает события
-        */
-        this.bindEvent = function () {
-            //Наведение на контейнер
-            this.addEvent('mousemove', el, paralax.action.move);
+        this.go = function () {
+            this.addEvent('mousemove', that, base.move);
         }
 
-        /*
-        * Действия
-        */
-        this.action = {
-            move: function (e) {
-                //находим координаты мыши
-                var cords = paralax.getXY(e), blockLeft, blockTop;
+        this.move = function (e) {
+            //находим координаты мыши
+            var cords = base.getXY(e), blockLeft, blockTop;
 
-                //Проходим по всем словам
-                for (var i = 0; i < ln; i++) {
-                    var loc = blocks[i],
-                        shift = loc.getAttribute('data-shift'); //коэффициет сдвига
+            //Проходим по всем словам
+            for (var i = 0; i < ln; i++) {
+                var loc = blocks[i],
+                    shift = loc.getAttribute('data-shift'); //коэффициет сдвига
 
-                    //Смещение по x
-                    blockLeft = shift * (0.5 * innerWidth - cords.mouseX);
-                    //Смешение по y
-                    blockTop = shift * (0.5 * innerHeight - cords.mouseY);
+                //Смещение по x
+                blockLeft = shift * (0.5 * innerWidth - cords.mouseX);
+                //Смешение по y
+                blockTop = shift * (0.5 * innerHeight - cords.mouseY);
 
-                    if (options.differentSides) {
-                        if (i % 2 === 0) {
-                            loc.style.cssText = paralax.getCssString(blockLeft, blockTop);
-                        } else {
-                            loc.style.cssText = paralax.getCssString(blockLeft, blockTop, -1);
-                        }
+                if (config.differentSides) {
+                    if (i % 2 === 0) {
+                        loc.style.cssText = base.getCssString(blockLeft, blockTop);
                     } else {
-                        loc.style.cssText = paralax.getCssString(blockLeft, blockTop);
+                        loc.style.cssText = base.getCssString(blockLeft, blockTop, -1);
                     }
+                } else {
+                    loc.style.cssText = base.getCssString(blockLeft, blockTop);
                 }
             }
-        };
+        }
+
         /*
         * Формирует css строку
         */
@@ -101,46 +142,44 @@
             return cssString;
         };
 
-        var paralax = this;
-    };
-    /*
-    * Регистрирует события
-    */
-    Paralax.prototype.addEvent = function (type, el, func) {
-        el.addEventListener(type, function (event) {
-            var e = event || w.event;
-            func.call(this, e);
-        }, false);
+        var base = this;
     };
 
-    /*
-    * Получение координат мыши
-    */
-    Paralax.prototype.getXY = function (e) {
-        //если IE 
-        if (!e.pageX) {
-            var html = d.documentElement;
-            var body = d.body;
+    BaseParalaxStr.prototype = Object.create(GetParalax.prototype);
 
-            e.pageX = e.clientX + (html && html.scrollLeft || body && body.scrollLeft || 0) - (html.clientLeft || 0);
-            e.pageY = e.clientY + (html && html.scrollTop || body && body.scrollTop || 0) - (html.clientTop || 0);
+    //Для горизонтального паралакса
+    var HorizontParalaxStr = function () {
+        this.go = function () {
+            
         }
-
-        return { 'mouseX': e.pageX, 'mouseY': e.pageY };
     };
+
+    HorizontParalaxStr.prototype = Object.create(GetParalax.prototype);
 
     $.fn.woolParalax = function (options) {
         //Дефолтовые настройки
         var defOption = {
+            'type': 'none',//none, horizont, vertical
+            'autoHeight': true,
             'layerClass': '.wool-layer',
+            'contentClass': '.wool-layer-content',
             'differentSides': true//Если установлено в true то четные и нечетные слои будут двигаться в разные чтороны
         };
 
         $.extend(defOption, options);
 
         return this.each(function () {
-            var paralax = new Paralax(this, defOption);
-            paralax.init();
+
+            switch (defOption.type) {
+                case 'none':
+                    var paralax = new Paralax(new BaseParalaxStr());   
+                    paralax.start(this, defOption); 
+                    break;
+                case 'horizont':
+                    var paralax = new Paralax(new HorizontParalaxStr());
+                    paralax.start(this, defOption);
+                    break;
+            }
         });
     }
 } (jQuery));
